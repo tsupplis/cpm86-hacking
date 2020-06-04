cc88e6ce7fa725395815a29d08ab2f7f3c349927
Thierry Supplisson <ts@js353.com>
2020-06-04 02:27:27 +0100

    removed test quit interrupt

--- pause.asm
+++ pause.asm
@@ -1,28 +1,23 @@
-                title   'Clear Screen'
+                title   'Pause for Key'
 
                 cseg     
                 mov     dx, offset msg_hdr
                 mov     cl,9            
                 int     0E0h    
-                jmp     quit          
 pause:
                 mov     ah,1
                 int     16h
-                jz      got_key
+                jnz     got_key
                 jmp     pause
 got_key:
                 mov     ah,0
                 int     16h
                 xor     ax,ax
-                mov     cl,9
                 mov     dx,offset msg_clear
+                mov     cl,9
                 int     0E0h
                 xor     cx,cx
                 int     0E0h
-
-quit:                
-                xor     cx,cx 
-                int     0E0h    
                
                 dseg    
                 org     100h
