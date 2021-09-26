0842c51587d93aada1e65d5b68052081427e8c4a
Thierry Supplisson <ts@js353.com>
2021-09-26 16:46:47 +0100

    import of *nix tools

---
+++ pce/build.bat
@@ -0,0 +1,11 @@
+@echo off
+rasm86 pcever $ pz sz
+linkcmd pcever[$sz]
+rasm86 pcetime $ pz sz
+linkcmd pcetime[$sz]
+rasm86 pcemnt $ pz sz
+linkcmd pcemnt[$sz]
+rasm86 pceinit $ pz sz
+linkcmd pceinit[$sz]
+rasm86 pceexit $ pz sz
+linkcmd pceexit[$sz]
