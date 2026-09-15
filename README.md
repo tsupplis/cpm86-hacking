c939a66434d7892f39c063408565e1596290bb3e
tsupplis <tsupplis@users.noreply.github.com>
2026-09-15 14:06:11 +0100

    dos/unix cleanup

--- README.md
+++ README.md
@@ -101,7 +101,7 @@ For the status, (\\s: space, \\\\: \\, \\u: upper, \\l: lower)
 - PAUSE: submit tool waiting for a keystroke
 - TOD: Replacement for CP/M-86 without the 78-99 year constraint and date/time validation including leap years. It does not fix the visual issue of the century hard coded to 19. Patches exist for that. It has exactly the same behaviour as the original CP/M-86 tod.cmd tool. (CP/M-86 1.1 for PC/XT only)
 - VER: Displays the BDOS version (DOSVER.COM is provided to display MS-DOS Compatibility on PC-MODE compatible OSes)
-- MEM: Displays the available and system memory
+- MEM: Displays the available and system memory (DOSMEM.COM and DOSMEM11.COM are provided to display infor in PC-MODE)
 - BALL: A simple CGA demo (CP/M-86 1.1 for PC/XT, DOS Plus and CCP/M-86 or Concurrent DOS BDOS > 3.1) (take on https://www.z80cpu.eu/mirrors/klaw/bouncy.zip)
 - GETCH: A simple keyboard scanner
 - AT clock tools
--- dosver.asm
+++ dosver.asm
@@ -1,65 +1,124 @@
-
 program  segment
-        assume cs:program, ds:program
-        org    100h
-_start:     
-        mov    cx,0Ch
-        int    0E0h
-        push   ax
-        lea    dx,[_header]
-        mov    ah, 09h
-        int    21h
-        pop    ax
-        cmp    al,41h
-        jb     _print_v1
-        mov    ah, 30h
-        int    21h
-        push   ax
-        xor    ah,ah
-        call   _print_dec
-        mov    dl, '.'
-        mov    ah, 02h
-        int    21h
-        pop    ax
-        mov    al,ah
-        xor    ah,ah
-        call   _print_dec
-        lea    dx,[_nl]
-        mov    ah, 09h
-        int    21h
-        jmp    _exit
-_print_v1:
-        lea    dx,[_v1]
-        mov    ah, 09h
-        int    21h
-_exit:
-        int    20h
-_print_dec:
-        mov    cx,0
-        mov    dx,0
-_print_dec_loop:
-        cmp    ax,0
-        je     _print_dec_1     
-        mov    bx,10       
-        div    bx                 
-        push   dx             
-        inc    cx             
-        xor    dx,dx
-        jmp    _print_dec_loop
-_print_dec_1:
-        cmp    cx,0
-        je     _print_dec_exit
-        pop    dx
-        add    dx,48
-        mov    ah,02h
-        int    21h
-        dec    cx
-        jmp    _print_dec_1
-_print_dec_exit:
-        ret
-            
-_header  db    "MS-DOS Level $"
-_v1      db    "1.1"
-_nl      db    13, 10, "$"
+         assume cs:program, ds:program
+            org    100h
+_start:
+            mov     dx, offset dos_version
+            call    print_string
+            mov     ah, 30h
+            int     21h
+            cmp     al, 1
+            jge     .chk_drdos
+            mov     ah, 9
+            lea     dx, olddos
+            int     21h
+            jmp     short .exit
+.chk_drdos:
+            xchg    al,ah
+            mov     bx,0
+            call    print_ver
+            mov     ax, 4452h
+            int     21h
+            jc      .chk_cdos
+            call    fix_dr_ver
+            mov     dx, offset dr_version
+            call    print_string
+            mov     bx,1
+            call    print_ver
+            jmp     .exit
+.chk_cdos:
+            mov     ax, 4451h
+            int     21h
+            jc      .exit
+            call    fix_dr_ver
+            mov     dx, offset cdos_version
+            call    print_string
+            mov     bx,1
+            call    print_ver
+.exit:
+            lea     dx, eol
+            call    print_string
+            int     20h
+
+print_string:
+            push    ax
+            mov     ah, 09h
+            int     21h
+            pop     ax
+            ret
+
+print_ver:
+            push    cx
+            push    dx
+            push    ax
+            mov     al,ah
+            xor     ah,ah
+            call    convert
+            mov     dl, '.'
+            mov     ah, 02h
+            int     21h
+            pop     ax
+            cmp     bx,1
+            je      .nz_minor
+            cmp     al, 10
+            jge     .nz_minor
+            push    ax
+            mov     ah, 2
+            mov     dl, '0'
+            int     21h
+            pop     ax
+.nz_minor:
+            xor     ah, ah
+            call    convert
+            pop     dx
+            pop     cx
+            ret
+
+convert:
+            push    ax
+            push    bx
+            push    dx
+            push    di
+            mov     bl, 10
+            mov     di, offset digitend
+.loop:
+            dec     di
+            div     bl
+            add     ah, '0'
+            mov     [di], ah
+            xor     ah, ah
+            test    al, al
+            jnz     short .loop
+            mov     ah, 9
+            mov     dx, di
+            int     21h
+            pop     di
+            pop     dx
+            pop     bx
+            pop     ax
+            ret
+
+fix_dr_ver:
+            push    cx
+            mov     ah, al
+            mov     cl,4
+            shr     ah, cl
+            and     ah,0Fh
+            and     al,0Fh
+            pop     cx
+            ret
+
+dos_version:
+            db "PC/MS-DOS $"
+cdos_version:
+            db 0Ah,0Dh,"Concurrent BDOS $"
+dr_version:
+            db 0Ah,0Dh,"DR BDOS $"
+olddos:
+            db "1.x0$"
+            db "000000"
+digitend:   
+            db "$"
+eol:
+            db 0Ah,0Dh,'$'
 program  ends
          end _start
