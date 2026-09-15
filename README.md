bb6d0b646549830615e3054ec013d5b500661466
tsupplis <tsupplis@users.noreply.github.com>
2026-09-15 19:53:59 +0100

    add psp create -c option to pspdump.com

--- README.md
+++ README.md
@@ -104,7 +104,7 @@ For the status, (\\s: space, \\\\: \\, \\u: upper, \\l: lower)
 - MEM: Displays the available and system memory (DOSMEM.COM and DOSMEM11.COM are provided to display infor in PC-MODE)
 - BALL: A simple CGA demo (CP/M-86 1.1 for PC/XT, DOS Plus and CCP/M-86 or Concurrent DOS BDOS > 3.1) (take on https://www.z80cpu.eu/mirrors/klaw/bouncy.zip)
 - GETCH: A simple keyboard scanner (DOSGETCH.COM is provided as DOS counterpart)
-- ZPDUMP: Dumps the Zero Page content (PSPDUMP.COM is provided as DOS counterpart)
+- ZPDUMP: Dumps the Zero Page content (PSPDUMP.COM is provided as DOS counterpart). PSPDUMP also takes a command line -C to perform an int 21h function 26h first and dumps the new PSP.
 - AT clock tools
     - ATTIME: Sync up clock (PC/XT with an AT compatible clock only)
     - ATINIT: Sync up clock and display boot banner (PC/XT with an AT compatible clock only)
--- pspdump.c
+++ pspdump.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void dump_hex(FILE *fd, const *doc, const void* data, unsigned int size) {
+void dump_hex(FILE *fd, char *doc, char* data, unsigned int size) {
 	char ascii[17];
 	unsigned int i, j;
 	ascii[16] = '\0';
@@ -30,26 +30,65 @@ void dump_hex(FILE *fd, const *doc, const void* data, unsigned int size) {
 	}
 }
 
-int main() {
+
+static char psp_buffer[256+16];
+static char * psp_target;
+
+#if !defined(__CPM86__)
+
+new_psp_target() {
+#asm
+	;push ax
+	push cx
+	push dx
+	mov ax, psp_target_
+	mov cl, 4
+	shr ax, cl
+	mov dx, ax
+	mov ax, cs
+	add dx, ax
+	mov ax, dx
+	mov ah, 26h
+	int 21h
+	pop dx
+	pop cx
+	;pop ax
+#endasm
+}
+
+#endif
+
+int main(int argc, char *argv[])
+ {
+	psp_target = 0;
 #if defined(__CPM86__)
-    dump_hex(stdout,"ZERO_PAGE",(void *)0x0000,92);
-    dump_hex(stdout,"8080",(void*)0x05,1);
-    dump_hex(stdout,"LOAD_DRIVE(CP/M>3.0)",(void*)0x50,01);
-    dump_hex(stdout,"FCB1",(void*)0x5C,12);
-    dump_hex(stdout,"FCB2",(void*)0x6C,12);
-    dump_hex(stdout,"TAIL_LENGTH",(void*)0x80,1);
-    dump_hex(stdout,"TAIL",(void*)0x81,127);
+    dump_hex(stdout,"ZERO_PAGE",psp_target,92);
+    dump_hex(stdout,"8080",psp_target+0x05,1);
+    dump_hex(stdout,"LOAD_DRIVE(CP/M>3.0)",psp_target+0x50,01);
+    dump_hex(stdout,"FCB1",psp_target+0x5C,12);
+    dump_hex(stdout,"FCB2",psp_target+0x6C,12);
+    dump_hex(stdout,"TAIL_LENGTH",psp_target+0x80,1);
+    dump_hex(stdout,"TAIL",psp_target+0x81,127);
 #else
-    dump_hex(stdout,"PSP",(void *)0x0000,92);
-    dump_hex(stdout,"CPMEXIT",(void *)0x0000,2);
-    dump_hex(stdout,"JMPTERM",(void*)0x05,5);
-    dump_hex(stdout,"ENVSEG",(void*)0x2C,2);
-    dump_hex(stdout,"VER(DOS>5.0)",(void*)0x40,2);
-    dump_hex(stdout,"JMPINT",(void*)0x50,3);
-    dump_hex(stdout,"FCB1",(void*)0x5C,12);
-    dump_hex(stdout,"FCB2",(void*)0x6C,12);
-    dump_hex(stdout,"ARGS_LENGTH",(void*)0x80,1);
-    dump_hex(stdout,"ARGS",(void*)0x81,127);
+	if(argc>1 && (!strcmp(argv[1],"-c") || !strcmp(argv[1],"-C"))) {
+		memset(psp_buffer, 0xDD, sizeof(psp_buffer));
+		fprintf(stdout,"Setting new PSP target...\n");
+		psp_target = psp_buffer;
+		psp_target += 16-((int)(psp_target) % 16);
+		new_psp_target();
+		dump_hex(stdout,"NEW_PSP",psp_target,92);
+	} else {
+		dump_hex(stdout,"NEW_PSP",psp_target,92);
+	}
+    dump_hex(stdout,"CPMEXIT",psp_target,2);
+    dump_hex(stdout,"JMPTERM",psp_target+0x05,5);
+    dump_hex(stdout,"ENVSEG",psp_target+0x2C,2);
+    dump_hex(stdout,"VER(DOS>5.0)",psp_target+0x40,2);
+    dump_hex(stdout,"JMPINT",psp_target+0x50,3);
+    dump_hex(stdout,"FCB1",psp_target+0x5C,12);
+    dump_hex(stdout,"FCB2",psp_target+0x6C,12);
+    dump_hex(stdout,"ARGS_LENGTH",psp_target+0x80,1);
+    dump_hex(stdout,"ARGS",psp_target+0x81,127);
 #endif
     return 0;
 }
